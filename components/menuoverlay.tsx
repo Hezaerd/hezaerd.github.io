26bb5d6b44bec64dc0f4e537f3a6f1b4b7f4558e
Swann <goldenland046@gmail.com>
2023-11-17 14:47:46 +0100

    Create menu overlay component

---
+++ components/menuoverlay.tsx
@@ -0,0 +1,25 @@
+import React, { FC } from 'react';
+import NavLink from './navlink';
+
+interface Link {
+  path: string;
+  title: string;
+}
+
+interface MenuOverlayProps {
+  links: Link[];
+}
+
+const MenuOverlay: FC<MenuOverlayProps> = ({ links }) => {
+  return (
+    <ul className="flex flex-col py-4 items-center">
+      {links.map((link, index) => (
+        <li key={index}>
+          <NavLink href={link.path} title={link.title} />
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default MenuOverlay;
