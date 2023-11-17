8d3f6a3e4ed174be67b2ec858139072bb7e3cd08
Swann <goldenland046@gmail.com>
2023-11-17 15:06:03 +0100

    New navbar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,46 +1,63 @@
-import { Fragment } from 'react';
-import { Disclosure, Menu, Transition } from '@headlessui/react';
-import { Bars3Icon, BellIcon, XMarkIcon} from '@heroicons/react/24/outline';
+import React, {useState} from "react";
+import Link from "next/link";
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/20/solid";
+import NavLink from "./navlink";
+import MenuOverlay from "./menuoverlay";
 
 const navigation = [
-    { name: 'Home', href: '#', current: true},
-    { name: 'About', href: '#', current: false},
-    { name: 'Portfolio', href: '#', current: false},
-    { name: 'Resume', href: '#', current : false},
+    { name: 'Home', href: '#'},
+    { name: 'About', href: '#'},
+    { name: 'Portfolio', href: '#'},
+    { name: 'Contact', href: "#"},
 ];
 
-function classNames(...classes: string[]) {
-    return classes.filter(Boolean).join(' ');
-}
-
 export default function Navbar() {
+    const [isOpen, setIsOpen] = useState(false);
+
     return (
-        <div className="min-h-full">
-            <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
-                <div className="flex h-16 items-center justify-between">
-                    <div className="flex items-center">
-                        <div className="hidden md:block">
-                            <div className="ml-10 flex items-baseline space-x-4">
-                                {navigation.map((item) => (
-                                    <a
+        <nav className="fixed top-0 left-0 right-0 z-10 bg-[#121212] bg-opacity-90">
+            <div className="flex flex-wrap items-center justify-between mx-auto p-8">
+                <Link
+                    href="/"
+                    className="text-2xl md:text-5xl text-white font-semibold"
+                >
+                </Link>
+                <div className="block md:hidden">
+                    {!isOpen ? (
+                        <button
+                            id="nav-toggle"
+                            className="flex items-center px-3 py-2 border rounded text-slate-200 border-slate-200 hover:text-white hover:border-white"
+                            onClick={() => setIsOpen(true)}
+                        >
+                            <Bars3Icon className="h-3 w-3" />
+                        </button>
+                        ) : (
+                        <button
+                            id="nav-toggle"
+                            className="flex items-center px-3 py-2 border rounded text-slate-200 border-slate-200 hover:text-white hover:border-white"
+                            onClick={() => setIsOpen(false)}
+                        >
+                        <XMarkIcon className="h-5 w-5" />
+                        </button>
+                        )}
+                </div>
+                <div className="hidden md:block md:w-auto" id="navbar-default">
+                    <ul className="font-medium flex p-4 md:p-0  rounded-lg flex-row md:space-x-8 mt-0">
+                        {navigation.map((item) => {
+                            return (
+                                <li>
+                                    <NavLink
                                         key={item.name}
+                                        title={item.name}
                                         href={item.href}
-                                        className={classNames(
-                                            item.current
-                                            ? 'bg-gray-900 text-white'
-                                            : 'text-gray-300 hover:bg-gray-700 hover:text-white',
-                                            'rounded-md px-3 py-2 text-sm font-medium'
-                                        )}
-                                        aria-current={item.current ? 'page' : undefined}
-                                    >
-                                        {item.name}
-                                    </a>
-                                ))}
-                            </div>                                
-                        </div>
-                    </div>
+                                    />
+                                </li>
+                            );
+                        })}
+                    </ul>
                 </div>
             </div>
-        </div>
+            {isOpen ? <MenuOverlay links={navigation} /> : null}
+        </nav>
     );
 }
