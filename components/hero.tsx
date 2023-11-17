9d412360a7ac694473280bf27f4f0cfc975b0a5e
Swann <goldenland046@gmail.com>
2023-11-17 16:08:26 +0100

    Fix type size to stop moving the whole page

--- components/hero.tsx
+++ components/hero.tsx
@@ -12,7 +12,7 @@ export default function Hero() {
     return (
         <section className="grid grid-cols-1 lg:grid-cols-12 my-4">
             <div className="col-span-7 place-self-center place-items-center grid lg:place-items-start">
-                <h1 className="text-white max-w-2xl mb-4 lg:text-6xl text-4xl font-extrabold">
+                <h1 className="text-white max-w-1.99xl mb-4 lg:text-6xl text-4xl font-extrabold">
                     <span className="text-transparent text-6xl bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600">
                         Hello, I'm{" "}
                     </span>{""}
