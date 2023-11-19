4e146810a1758aba9cf1a3136e5f5b71f397d6ee
Swann <goldenland046@gmail.com>
2023-11-19 18:42:56 +0100

    Add a contact component

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,7 @@ import Navbar_Section from "@/components/sections/navbar-section";
 import About_Section from "@/components/sections/about-section";
 import Projects_Section from "@/components/sections/projects-section";
 import Footer_Section from "@/components/sections/footer-section";
+import Contact_Section from "@/components/sections/contact-section";
 
 import "@/app/globals.css";
 
@@ -14,6 +15,7 @@ export default function Home() {
                 <Hero_Section />
                 <About_Section />
                 <Projects_Section />
+                <Contact_Section />
             </div>
             <Footer_Section />
         </main>
